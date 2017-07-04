9f63a15efe5f973e1c79efae62125577852a82f3
Bourdel Maxime <bourdel.maxime.pro@gmail.com>
2017-07-04 20:51:00 +0200

    MBO - Users have to be registered to see pages + Adding some Style

---
+++ src/MainBundle/Entity/Calendrier.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace MainBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Calendrier
+ *
+ * @ORM\Table(name="calendrier")
+ * @ORM\Entity(repositoryClass="MainBundle\Repository\CalendrierRepository")
+ */
+class Calendrier
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="date", unique=true)
+     */
+    private $date;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="jour", type="integer")
+     */
+    private $jour;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="mois", type="integer")
+     */
+    private $mois;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="annee", type="integer")
+     */
+    private $annee;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="semaine", type="integer")
+     */
+    private $semaine;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set date
+     *
+     * @param \DateTime $date
+     *
+     * @return Calendrier
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * Get date
+     *
+     * @return \DateTime
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * Set jour
+     *
+     * @param integer $jour
+     *
+     * @return Calendrier
+     */
+    public function setJour($jour)
+    {
+        $this->jour = $jour;
+
+        return $this;
+    }
+
+    /**
+     * Get jour
+     *
+     * @return int
+     */
+    public function getJour()
+    {
+        return $this->jour;
+    }
+
+    /**
+     * Set mois
+     *
+     * @param integer $mois
+     *
+     * @return Calendrier
+     */
+    public function setMois($mois)
+    {
+        $this->mois = $mois;
+
+        return $this;
+    }
+
+    /**
+     * Get mois
+     *
+     * @return int
+     */
+    public function getMois()
+    {
+        return $this->mois;
+    }
+
+    /**
+     * Set annee
+     *
+     * @param integer $annee
+     *
+     * @return Calendrier
+     */
+    public function setAnnee($annee)
+    {
+        $this->annee = $annee;
+
+        return $this;
+    }
+
+    /**
+     * Get annee
+     *
+     * @return int
+     */
+    public function getAnnee()
+    {
+        return $this->annee;
+    }
+
+    /**
+     * Set semaine
+     *
+     * @param integer $semaine
+     *
+     * @return Calendrier
+     */
+    public function setSemaine($semaine)
+    {
+        $this->semaine = $semaine;
+
+        return $this;
+    }
+
+    /**
+     * Get semaine
+     *
+     * @return int
+     */
+    public function getSemaine()
+    {
+        return $this->semaine;
+    }
+}
+
