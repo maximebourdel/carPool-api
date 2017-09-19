28cbcbd205d95fdb8def8bd46e4e1781fd519013
maximebourdel <bourdel.maxime.pro@gmail.com>
2017-09-19 21:54:38 +0200

    MBO - Fixing bug for creneaux

--- src/MainBundle/Repository/ReservationRepository.php
+++ src/MainBundle/Repository/ReservationRepository.php
@@ -41,7 +41,23 @@ class ReservationRepository extends \Doctrine\ORM\EntityRepository
 
     /**
      * Retourne les creneaux par vehicules pour une annee et un mois donne
-    **/
+     * 
+       SELECT 
+           vehi.immatriculation
+           , cal.jour
+           , sum(case when cal.date between res.date_debut and res.date_fin then 1 else 0 end ) is_reserve
+           , max(case when cal.date between res.date_debut and res.date_fin then res.nom else null end ) nom
+           , max(case when cal.date between res.date_debut and res.date_fin then res.prenom else null end ) prenom
+       FROM calendrier cal, vehicule vehi
+           LEFT JOIN reservation res on vehi.id = res.vehicule_id
+               AND res.statut != 'Annulée' 
+       WHERE cal.annee = 2017 AND cal.mois = 09 
+       GROUP BY vehi.immatriculation, cal.jour
+       ORDER BY vehi.immatriculation, cal.jour 
+     * @param int $annee année du créneau
+     * @param int $mois mois du créneau
+     * @return type des créneaux sur un mois
+     */
     public function findCreneauxByAnneeMois(int $annee, int $mois)
     {
 
@@ -51,13 +67,13 @@ class ReservationRepository extends \Doctrine\ORM\EntityRepository
                     vehi.immatriculation
                     , cal.jour
                     , sum(case when cal.date between res.dateDebut and res.dateFin then 1 else 0 end ) is_reserve
-                    , res.nom
-                    , res.prenom
+                    , max(case when cal.date between res.dateDebut and res.dateFin then res.nom else \'\' end ) nom 
+                    , max(case when cal.date between res.dateDebut and res.dateFin then res.prenom else \'\' end ) prenom 
                 FROM MainBundle:Calendrier cal, MainBundle:Vehicule vehi
                     LEFT JOIN vehi.reservations res 
                         WITH res.statut != \'Annulée\' 
-                WHERE cal.annee = '. $annee .' AND cal.mois = '. $mois .' 
-                GROUP BY vehi.immatriculation, cal.jour, res.nom, res.prenom 
+                WHERE cal.annee = '. $annee .' AND cal.mois = '. $mois .'
+                GROUP BY vehi.immatriculation, cal.jour 
                 ORDER BY vehi.immatriculation, cal.jour'
             )
             ->getResult();
